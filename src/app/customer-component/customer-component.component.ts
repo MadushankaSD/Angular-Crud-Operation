5f6a67a2858c3ace1f452c1cc825091158bc1b26
madushanka kumara <oska.coc@gmail.com>
2020-06-08 20:27:00 +0530

    Crud Operation Done CustomerController

--- src/app/customer-component/customer-component.component.ts
+++ src/app/customer-component/customer-component.component.ts
@@ -10,6 +10,16 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 export class CustomerComponentComponent implements OnInit{
 
+id;
+name;
+address;
+
+ number;
+
+save="Save";
+
+ tablelick:boolean;
+
   formName = new FormGroup({
     id:new FormControl(),
     name:new FormControl(),
@@ -36,15 +46,41 @@ export class CustomerComponentComponent implements OnInit{
       .subscribe(val=>{
         let number = this.customers.indexOf(customer);
         this.customers.splice(number,1);
+        this.formName.reset();
       });
   }
 
   saveCustomer() {
+    if(this.tablelick){
 
-      this.http.post('http://localhost:8080/posweb/api/v1/customers', this.formName.value)
+      this.http.put(this.url, this.formName.value,{params:{id:this.formName.value.id}})
+        .subscribe(response => {
+          this.customers.splice(this.number,1,this.formName.value)
+          this.formName.reset();
+          this.tablelick=false;
+          this.save="save";
+        });
+
+    }else {
+      this.http.post(this.url, this.formName.value)
         .subscribe(response => {
           this.customers.push(this.formName.value);
           this.formName.reset();
         });
+      }
     }
+
+  tableClick(customer) {
+    this.save="update"
+    this.tablelick=true;
+    this.number = this.customers.indexOf(customer);
+    this.id=customer.id;
+     this.name=customer.name;
+     this.address=customer.address;
+  }
+
+  clearClick() {
+    this.save="save"
+    this.tablelick=false;
+  }
 }
